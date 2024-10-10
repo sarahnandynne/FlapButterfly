ffed4fbb574994f3255af6c600acc91d72791667
SarahNandynne <miguelsarah051@gmail.com>
2024-10-10 08:50:16 -0300

    dia 10/10

--- FlapButterfly/MainPage.xaml.cs
+++ FlapButterfly/MainPage.xaml.cs
@@ -2,10 +2,30 @@
 
 public partial class MainPage : ContentPage
 {
+    const int Gravidade = 50;
+    const int TempoEntreFrames = 1000;
+    bool EstaMorto = false;
     public MainPage()
     {
 	InitializeComponent();
     }
     
+    void AplicaGravidade()
+    {
+        imgBorboleta.TranslationY += Gravidade;
+    } 
+    async Task Desenhar()
+    {
+        while (!EstaMorto)
+        {
+            AplicaGravidade();
+            await Task.Delay(TempoEntreFrames);
+        }
+    } 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        Desenhar();
+    }
 }
 
