8d314593f767deafc634036361de51998f0a09e6
SarahNandynne <miguelsarah051@gmail.com>
2024-10-11 10:55:23 -0300

    dia 11/10

--- FlapButterfly/MainPage.xaml.cs
+++ FlapButterfly/MainPage.xaml.cs
@@ -2,9 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-    const int Gravidade = 50;
-    const int TempoEntreFrames = 1000;
-    bool EstaMorto = false;
+    const int Gravidade = 30;
+    const int TempoEntreFrames = 100;
+    bool EstaMorto = true;
+    double LarguraJanela = 0;
+    double AlturaJanela = 0;
+    double AlturaCano = 300;
+    double AlturaJanelaInicial = 0;
+    int Velocidade = 20;
     public MainPage()
     {
 	InitializeComponent();
@@ -20,12 +25,43 @@ public partial class MainPage : ContentPage
         {
             AplicaGravidade();
             await Task.Delay(TempoEntreFrames);
+            GerenciaCanos();
         }
     } 
-    protected override void OnAppearing()
+    protected override void OnSizeAllocated(double w, double h)
     {
-        base.OnAppearing();
+        base.OnSizeAllocated(w, h);
+        LarguraJanela = w;
+        AlturaJanela = h;
+        if (AlturaJanelaInicial > 0)
+        {
+            imgCanoCima.HeightRequest = AlturaCano * h / AlturaJanelaInicial;
+            imgCanoBaixo.HeightRequest = AlturaCano * h / AlturaJanelaInicial;
+        }
+        else 
+            AlturaJanelaInicial = h;
+    }
+
+    void GerenciaCanos()
+    {
+        imgCanoCima.TranslationX -= Velocidade;
+        imgCanoBaixo.TranslationX -= Velocidade;
+        if (imgCanoBaixo.TranslationX <= -LarguraJanela)
+        {
+            imgCanoBaixo.TranslationX = 0;
+            imgCanoCima.TranslationX = 0;
+        }
+    }
+    void GameOverClicked (object s, TappedEventArgs a)
+    {
+        frameGameOver.IsVisible = false;
+        Inicializar();
         Desenhar();
+    }
+    void Inicializar()
+    {
+        EstaMorto = false;
+        imgBorboleta.TranslationY = 0;
     }
 }
 
